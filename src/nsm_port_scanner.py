5991db1d9ad73facfa41207773185381b8300fcc
Jabari <jabarilucien@gmail.com>
2026-03-15 17:59:39 -0400

    partial

--- src/nsm_port_scanner.py
+++ src/nsm_port_scanner.py
@@ -113,7 +113,7 @@ class Socket_Port_Scanner():
             cls.total_ips_scanned += 1
             # cls._threader_ports(ip=ip, timeout=timeout)
             threading.Thread(target=cls._threader_ports, args=(ip, timeout), daemon=True).start()
-            console.print(f"]\n[bold green][+] Scanning:[yellow] {ip}")
+            console.print(f"\n[bold green][+] Scanning:[yellow] {ip}")
 
 
     
